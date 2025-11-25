ece6af0e0d0758a13ffe568950452b9693424c79
Рахат Тукназаров <tuknazarovrakhat06@gmail.com>
2025-11-25 07:31:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9250948810-7d1bb",
+  "appId": "1:513534762812:web:92d3c89c246ea4679e4839",
+  "apiKey": "AIzaSyCWXa78Mh9KmEQ0DcSRr0idXVTNXR14SA8",
+  "authDomain": "studio-9250948810-7d1bb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "513534762812"
+};
